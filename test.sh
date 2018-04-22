c94e9eeb269b09e508dda4a7e71fa1ee502d4925
nicolsek <tusek.nicole@gmail.com>
2018-04-21 17:15:56 -0700

    Attempt at updating travis to not halt forever.

--- test.sh
+++ test.sh
@@ -1,11 +1,10 @@
 cat config.json # Confirms file exists.
 python server.py & # Starts server in background.
+
+wait %1
+
 python serve.py https://github.com/mdn/beginner-html-site-styled & # Sending git url.
 
 wait %2
 
-ls repo/ & # Confirm the repo folder exists.
-
-wait %3
-
-killall python
+ls repo/ # Confirm the repo folder exists.
